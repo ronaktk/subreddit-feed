abdd06abf9eb873c1600e00d09b6207f4cd6eed6
Ronak Patel <ronaktk@gmail.com>
2022-02-21 17:26:42 -0600

    display create time

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import React, {useState, useEffect} from 'react';
-import Article from './components/Article';
+import React, {useState, useEffect} from 'react'
+import Article from './components/Article'
+import { GoLinkExternal } from 'react-icons/go'
 
 function App() {
 
-  const [articles, setArticles] = useState([]);
-  const [subreddit, setSubreddit] = useState('worldnews');
+  const [articles, setArticles] = useState([])
+  const [subreddit, setSubreddit] = useState('worldnews')
 
   useEffect(() => {
     fetch("https://www.reddit.com/r/" + subreddit + ".json").then(res => {
       if(res.status != 200) {
-        console.log("Error");
-        return;
+        console.log("Error")
+        return
       }
       res.json().then(data => {
-        if(data != null) {
-          console.log(data);
-          setArticles(data.data.children);
-        }
+        if(data != null) setArticles(data.data.children)
       })
 
     })
-  }, [subreddit]);
+  }, [subreddit])
 
   return (
     <div className="App">
       <header className="App-header">
         <input type="text" className="input" value={subreddit} onChange={e => setSubreddit(e.target.value)}/>
-      </header>
+          <a className="url" href={"https://reddit.com/r/"+subreddit} target="_blank"><GoLinkExternal/>
+          </a>
+        </header>
       <div className="articles">
         {
           (articles != null) ? 
-            articles.map((article, index) => <Article key={index} article={article.data} />) 
+            articles.map((article, index) => <Article key={index} article={article.data}/>) 
             : ''
         }
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React from "react"
+import timeSince from '../resources/timeSince'
 
 function Article(props) {
     return(
         <article>
             <a href={ "https://reddit.com" + props.article.permalink } target="_blank">
                 <h3>{props.article.title}</h3>
+                <span>{(timeSince(props.article.created))} ago</span>
             </a>
         </article>
     )
 }
 
-export default Article;
+export default Article
